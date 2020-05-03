7f99351338274434fd84b43598548af1a56c1171
Taylor <taylor.ono@cgi.com>
2020-05-03 14:02:52 -0700

    Working JSON diff report

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var _ = require('lodash'), handlebars = require('handlebars'), fs = require('fs'), path = require('path'), FILE_READ_OPTIONS = { encoding: 'utf8' }, DARK_THEME = 'diff-dashboard.hbs';
+var _ = require('lodash'), handlebars = require('handlebars'), util = require('./util'), fs = require('fs'), path = require('path'), FILE_READ_OPTIONS = { encoding: 'utf8' }, DARK_THEME = 'smoke-break-dashboard.hbs';
 /**
  * Reporter that outputs basic logs to DIFF (default: newman-run-report.csv).
  *
@@ -12,39 +12,101 @@ var _ = require('lodash'), handlebars = require('handlebars'), fs = require('fs'
 module.exports = function newmanDiffReporter(newman, options) {
     const htmlTemplate = path.join(__dirname, DARK_THEME);
     const compiler = handlebars.compile(fs.readFileSync(htmlTemplate, FILE_READ_OPTIONS));
+    newman.on('assertion', function (err, o) {
+        if (err) {
+            return;
+        }
+        if (o.skipped) {
+            newman.summary.skippedTests = newman.summary.skippedTests || [];
+            newman.summary.skippedTests.push({
+                cursor: {
+                    ref: o.cursor.ref,
+                    iteration: o.cursor.iteration,
+                    scriptId: o.cursor.scriptId
+                },
+                assertion: o.assertion,
+                skipped: o.skipped,
+                error: o.error,
+                item: {
+                    id: o.item.id,
+                    name: o.item.name
+                }
+            });
+        }
+    });
     newman.on('beforeDone', (err, event) => {
         if (err)
             return;
-        console.log("++++++++++++++++++++++++++++++");
-        console.log(event);
-        console.log("++++++++++++++++++++++++++++++");
-        console.log(event.summary.run);
         let executions = event.summary.run.executions;
-        let executionAs = _.filter(executions, function (e) { return e.cursor.iteration === 0; });
-        let executionBs = _.filter(executions, function (e) { return e.cursor.iteration === 1; });
+        executions.forEach((e) => {
+            if (e.response && typeof (e.response.toJSON) === 'function') {
+                e.response = e.response.toJSON();
+                let stream = e.response.stream;
+                if (stream) {
+                    e.response.body = Buffer.from(stream).toString();
+                }
+            }
+        });
+        let executionAs = _.filter(executions, function (e) {
+            return e.cursor.iteration === 0;
+        });
+        let executionBs = _.filter(executions, function (e) {
+            return e.cursor.iteration === 1;
+        });
         let Results = [];
         event.summary.collection.forEachItem((i) => {
-            var _a, _b;
-            let executionA = executionAs.find((e) => { return i.id === e.id; });
-            let executionB = executionBs.find((e) => { return i.id === e.id; });
+            let executionA = executionAs.find((e) => {
+                return i.id === e.id;
+            });
+            let executionB = executionBs.find((e) => {
+                return i.id === e.id;
+            });
             Results.push({
                 id: i.id,
                 name: i.name,
-                sampleA: { requestUrl: executionA.request.url.toString(), response: { status: executionA.response.status, code: executionA.response.code, body: (_a = executionA.response.stream) === null || _a === void 0 ? void 0 : _a.toString() } },
-                sampleB: { requestUrl: executionB.request.url.toString(), response: { status: executionB.response.status, code: executionB.response.code, body: (_b = executionB.response.stream) === null || _b === void 0 ? void 0 : _b.toString() } },
+                sampleA: {
+                    requestUrl: executionA.request.url.toString(),
+                    response: {
+                        status: executionA.response.status,
+                        code: executionA.response.code,
+                        body: executionA.response.body
+                    }
+                },
+                sampleB: {
+                    requestUrl: executionB.request.url.toString(),
+                    response: {
+                        status: executionB.response.status,
+                        code: executionB.response.code,
+                        body: executionB.response.body
+                    }
+                },
             });
         });
-        Results.forEach((r) => {
-            console.log(r.name);
-            console.log(r.sampleA.requestUrl, '\t', r.sampleA.response.code, '\t', r.sampleA.response.body);
-            console.log(r.sampleB.requestUrl, '\t', r.sampleB.response.code, '\t', r.sampleB.response.body);
-            console.log('\n');
-        });
+        let complied = {
+            skipHeaders: [],
+            skipSensitiveData: false,
+            omitHeaders: false,
+            noSyntaxHighlighting: false,
+            timestamp: Date(),
+            version: undefined,
+            aggregations: Results,
+            summary: {
+                stats: event.summary.run.stats,
+                collection: event.summary.collection,
+                globals: typeof (event.summary.globals) === 'object' ? event.summary.globals : undefined,
+                environment: event.summary.environment,
+                failures: event.summary.run.failures,
+                responseTotal: util.filesize(event.summary.run.transfers.responseTotal),
+                responseAverage: util.prettyms(event.summary.run.timings.responseAverage),
+                duration: util.prettyms(event.summary.run.timings.completed - event.summary.run.timings.started),
+                skippedTests: undefined
+            }
+        };
         newman.exports.push({
-            name: 'html-reporter-htmlextra',
-            default: 'newman_htmlextra.html',
+            name: 'html-reporter-smoke-break',
+            default: event.summary.collection.name + '-smoke-break.html',
             path: "",
-            content: compiler(Results)
+            content: compiler(complied)
         });
     });
 };
--- src/index.ts
+++ src/index.ts
@@ -2,152 +2,217 @@ import {Collection, ItemDefinition, Request, Response, VariableScopeDefinition} 
 import {NewmanRunOptions} from "newman";
 
 var _ = require('lodash'),
-handlebars = require('handlebars'),
-fs = require('fs'),
-path = require('path'),
-FILE_READ_OPTIONS = { encoding: 'utf8' },
-DARK_THEME = 'diff-dashboard.hbs'
+    handlebars = require('handlebars'),
+    util = require('./util'),
+    fs = require('fs'),
+    path = require('path'),
+    FILE_READ_OPTIONS = {encoding: 'utf8'},
+    DARK_THEME = 'smoke-break-dashboard.hbs'
 
 export interface NewmanSummary {
-  cursor:  Cursor;
-  summary: Summary;
+    cursor: Cursor;
+    summary: Summary;
 }
 
 export interface Cursor {
-  position:       number;
-  iteration:      number;
-  length:         number;
-  cycles:         number;
-  empty?:         boolean;
-  eof?:           boolean;
-  bof?:           boolean;
-  cr?:            boolean;
-  ref:            string;
-  httpRequestID?: string;
+    position: number;
+    iteration: number;
+    length: number;
+    cycles: number;
+    empty?: boolean;
+    eof?: boolean;
+    bof?: boolean;
+    cr?: boolean;
+    ref: string;
+    httpRequestID?: string;
 }
 
 export interface Summary {
-  collection: Collection;
-  environment: VariableScopeDefinition;
-  globals: VariableScopeDefinition;
-  run: Run;
+    collection: Collection;
+    environment: VariableScopeDefinition;
+    globals: VariableScopeDefinition;
+    run: Run;
 }
 
 export interface Run {
-  stats:      Stats;
-  timings:    { [key: string]: number };
-  executions: Execution[];
-  transfers:  Transfers;
-  failures:   any[];
-  error:      null;
+    stats: Stats;
+    timings: { [key: string]: number };
+    executions: Execution[];
+    transfers: Transfers;
+    failures: any[];
+    error: null;
 }
 
 export interface Execution {
-  cursor:     Cursor;
-  item:       ItemDefinition;
-  request:    Request;
-  response:   Response;
-  id:         string;
-  assertions: Assertion[];
+    cursor: Cursor;
+    item: ItemDefinition;
+    request: Request;
+    response: Response;
+    id: string;
+    assertions: Assertion[];
 }
 
 export interface Assertion {
-  assertion: string;
-  skipped: boolean;
+    assertion: string;
+    skipped: boolean;
 }
 
 export interface Stats {
-  iterations:        Assertions;
-  items:             Assertions;
-  scripts:           Assertions;
-  prerequests:       Assertions;
-  requests:          Assertions;
-  tests:             Assertions;
-  assertions:        Assertions;
-  testScripts:       Assertions;
-  prerequestScripts: Assertions;
+    iterations: Assertions;
+    items: Assertions;
+    scripts: Assertions;
+    prerequests: Assertions;
+    requests: Assertions;
+    tests: Assertions;
+    assertions: Assertions;
+    testScripts: Assertions;
+    prerequestScripts: Assertions;
 }
 
 export interface Assertions {
-  total:   number;
-  pending: number;
-  failed:  number;
+    total: number;
+    pending: number;
+    failed: number;
 }
 
 export interface Transfers {
-  responseTotal: number;
+    responseTotal: number;
 }
 
 
 interface NewmanResponse {
-  status: string
-  code: number
-  body?: string
+    status: string
+    code: number
+    body?: string
 }
 
 interface RequestResult {
-  requestUrl: string
-  response: NewmanResponse
+    requestUrl: string
+    response: NewmanResponse
 }
 
 export interface RequestDiff {
-  id:  string;
-  name: string;
-  sampleA: RequestResult
-  sampleB: RequestResult
+    id: string;
+    name: string;
+    sampleA: RequestResult
+    sampleB: RequestResult
 
 }
 
 /**
- * Reporter that outputs basic logs to DIFF (default: newman-run-report.csv).
+ * Reporter that outputs basic a HTML Report for smoke-break script
  *
  * @param {Object} newman - The collection run object, with event hooks for reporting run details.
  * @param {Object} options - A set of collection run options.
- * @param {String} options.export - The path to which the summary object must be written.
  * @returns {*}
  */
+module.exports = function newmanSmokeBreakReporter(newman: any, options: NewmanRunOptions) {
+    const htmlTemplate = path.join(__dirname, DARK_THEME);
+    const compiler = handlebars.compile(fs.readFileSync(htmlTemplate, FILE_READ_OPTIONS));
 
-module.exports = function newmanDiffReporter (newman: any, options: NewmanRunOptions) {
-  const htmlTemplate = path.join(__dirname, DARK_THEME);
-  const compiler = handlebars.compile(fs.readFileSync(htmlTemplate, FILE_READ_OPTIONS));
+    newman.on('assertion', function (err: boolean, o: { skipped: any; cursor: { ref: any; iteration: any; scriptId: any; }; assertion: any; error: any; item: { id: any; name: any; }; }) {
+        if (err) { return; }
 
-  newman.on('beforeDone', (err: any, event: NewmanSummary) => {
-    if (err) return
+        if (o.skipped) {
+            newman.summary.skippedTests = newman.summary.skippedTests || [];
 
-    console.log("++++++++++++++++++++++++++++++")
-    console.log(event)
-
-    console.log("++++++++++++++++++++++++++++++")
-    console.log(event.summary.run)
-
-    let executions = event.summary.run.executions
-    let executionAs = _.filter(executions, function(e:any) { return e.cursor.iteration ===0; });
-    let executionBs = _.filter(executions, function(e:any) { return e.cursor.iteration ===1; });
-
-    let Results: RequestDiff[] = []
-    event.summary.collection.forEachItem((i) => {
-      let executionA:Execution = executionAs.find((e:Execution) => {return i.id === e.id})
-      let executionB:Execution = executionBs.find((e:Execution) => {return i.id === e.id})
-      Results.push({
-        id: i.id,
-        name: i.name,
-        sampleA: {requestUrl: executionA.request.url.toString(), response:{status: executionA.response.status, code:executionA.response.code, body: executionA.response.stream?.toString()}},
-        sampleB: {requestUrl: executionB.request.url.toString(), response:{status: executionB.response.status, code:executionB.response.code, body: executionB.response.stream?.toString()}},
-      })
-    })
-
-    Results.forEach((r:RequestDiff) => {
-      console.log(r.name)
-      console.log(r.sampleA.requestUrl,'\t',r.sampleA.response.code ,'\t', r.sampleA.response.body)
-      console.log(r.sampleB.requestUrl,'\t',r.sampleB.response.code ,'\t', r.sampleB.response.body)
-      console.log('\n')
-    })
-
-    newman.exports.push({
-      name: 'html-reporter-htmlextra',
-      default: 'newman_htmlextra.html',
-      path: "",
-      content: compiler(Results)
+            newman.summary.skippedTests.push({
+                cursor: {
+                    ref: o.cursor.ref,
+                    iteration: o.cursor.iteration,
+                    scriptId: o.cursor.scriptId
+                },
+                assertion: o.assertion,
+                skipped: o.skipped,
+                error: o.error,
+                item: {
+                    id: o.item.id,
+                    name: o.item.name
+                }
+            });
+        }
     });
-  })
+
+
+    newman.on('beforeDone', (err: any, event: NewmanSummary) => {
+        if (err) return
+
+        let executions = event.summary.run.executions
+
+        executions.forEach((e:Execution)=> {
+            if (e.response && typeof (e.response.toJSON) === 'function') {
+                e.response = e.response.toJSON();
+                let stream = e.response.stream;
+                if (stream) {
+                    e.response.body = Buffer.from(stream).toString();
+                }
+            }
+        })
+
+        let executionAs = _.filter(executions, function (e: any) {
+            return e.cursor.iteration === 0;
+        });
+
+        let executionBs = _.filter(executions, function (e: any) {
+            return e.cursor.iteration === 1;
+        });
+
+        let Results: RequestDiff[] = []
+        event.summary.collection.forEachItem((i) => {
+            let executionA: Execution = executionAs.find((e: Execution) => {
+                return i.id === e.id
+            })
+            let executionB: Execution = executionBs.find((e: Execution) => {
+                return i.id === e.id
+            })
+            Results.push({
+                id: i.id,
+                name: i.name,
+                sampleA: {
+                    requestUrl: executionA.request.url.toString(),
+                    response: {
+                        status: executionA.response.status,
+                        code: executionA.response.code,
+                        body: executionA.response.body
+                    }
+                },
+                sampleB: {
+                    requestUrl: executionB.request.url.toString(),
+                    response: {
+                        status: executionB.response.status,
+                        code: executionB.response.code,
+                        body: executionB.response.body
+                    }
+                },
+            })
+        })
+
+        let complied = {
+            skipHeaders: [],
+            skipSensitiveData: false,
+            omitHeaders: false,
+            noSyntaxHighlighting: false,
+            timestamp: Date(),
+            version: undefined,
+            aggregations: Results,
+            summary: {
+                stats: event.summary.run.stats,
+                collection: event.summary.collection,
+                globals: typeof (event.summary.globals) === 'object' ? event.summary.globals : undefined,
+                environment: event.summary.environment,
+                failures: event.summary.run.failures,
+                responseTotal: util.filesize(event.summary.run.transfers.responseTotal),
+                responseAverage: util.prettyms(event.summary.run.timings.responseAverage),
+                duration: util.prettyms(event.summary.run.timings.completed - event.summary.run.timings.started),
+                skippedTests: undefined
+            }
+        }
+
+        newman.exports.push({
+            name: 'html-reporter-smoke-break',
+            default: event.summary.collection.name + '-smoke-break.html',
+            path: "",
+            content: compiler(complied)
+        });
+    })
 }
